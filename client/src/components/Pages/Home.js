bfc15dee81fd74c17da66e0604a8a41b5cc9d529
Will Zhang <46132945+notwz@users.noreply.github.com>
2021-09-25 16:23:07 -0400

    Update Home.js

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Home() {
-
-  return (
-    <div>
-      {" "}
-      <Link to={"/drive#" + "Shanghai"}> Go Driving </Link>
-      <span class="blink_me"> Exploration.. </span>
-      <span id="left"><div class="fadein"> fading in</div></span>
-
-      
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
